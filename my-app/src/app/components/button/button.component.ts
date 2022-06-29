27047ad7d7cafb1267cea92f0f3396a5cad4395d
Sushant Sharma <ssushantlb08@gmail.com>
2022-06-29 12:44:40 +0530

    learning day3

--- my-app/src/app/components/button/button.component.ts
+++ my-app/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input , Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,10 +10,17 @@ export class ButtonComponent implements OnInit {
   text!: string;
   @Input()
   color!: string;
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+    onClick(){
+      this.btnClick.emit();
+    }
+    toggleAddTask(){
+      console.log('toggle');
+      
+    }
 }
--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   title = 'Sushant';
   constructor() {}
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  toggleAddTask(){
+    console.log('toggle');
+    
+  }
 }
